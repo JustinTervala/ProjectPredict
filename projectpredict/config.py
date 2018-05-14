46512a65348a7f195d4ece82aeaa0812aa188665
JustinTervala <Tervala_Justin@bah.com>
2018-05-14 16:37:54 -0700

    Removed webserver. Relevant files are still contained in webserver branch

--- projectpredict/config.py
+++
@@ -1,8 +0,0 @@
-
-
-class Config(object):
-    repository = 'mongodb'
-    serialization = 'marshmallow'
-    model_serialization = 'dill'
-    model_path = 'projectpredict.learningmodels.gpy.Model'
-    recommendation_score_func = 'projectpredict.models.project.Project._default_recommendation_score_func'
